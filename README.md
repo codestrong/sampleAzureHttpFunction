56902b64760e8c9975cf28c7906cd875d69409c3
Anton Rukhlin <anton.rukhlin@codestrong.biz>
2018-05-01 12:28:17 +0300

    added sample function code

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # sampleAzureHttpFunction
+
+When I run this function with `mvn clean package azure-functions:run` it generates the following output:
+
+```
+[5/1/18 9:26:28 AM] Function started (Id=3af61181-70da-4e0a-98ac-fb4bc99d2efe)
+[5/1/18 9:26:28 AM] Executing 'Functions.myFunction' (Reason='This function was programmatically called via the host APIs.', Id=3af61181-70da-4e0a-98ac-fb4bc99d2efe)
+[5/1/18 9:26:28 AM] Java HTTP trigger processed a request.
+```
---
+++ src/main/java/biz/codestrong/jscraper/MyFunction.java
@@ -0,0 +1,55 @@
+package biz.codestrong.jscraper;
+
+import com.microsoft.azure.serverless.functions.ExecutionContext;
+import com.microsoft.azure.serverless.functions.HttpRequestMessage;
+import com.microsoft.azure.serverless.functions.HttpResponseMessage;
+import com.microsoft.azure.serverless.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.serverless.functions.annotation.FunctionName;
+import com.microsoft.azure.serverless.functions.annotation.HttpTrigger;
+import com.microsoft.azure.storage.CloudStorageAccount;
+import com.microsoft.azure.storage.OperationContext;
+import com.microsoft.azure.storage.blob.BlobContainerPublicAccessType;
+import com.microsoft.azure.storage.blob.BlobRequestOptions;
+import com.microsoft.azure.storage.blob.CloudBlobClient;
+import com.microsoft.azure.storage.blob.CloudBlobContainer;
+import com.microsoft.azure.storage.blob.CloudBlockBlob;
+
+/**
+ * Azure Functions with HTTP Trigger.
+ */
+public class MyFunction {
+
+	@FunctionName("myFunction")
+	public HttpResponseMessage<String> hello(@HttpTrigger(name = "request", authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<String> request,
+			final ExecutionContext context) {
+
+		context.getLogger().info("Java HTTP trigger processed a request.");
+
+		String requestBody = request.getBody();
+
+		CloudStorageAccount storageAccount;
+		CloudBlobClient blobClient = null;
+		CloudBlobContainer container = null;
+
+		try {
+			storageAccount = CloudStorageAccount.parse("UseDevelopmentStorage=true");
+			blobClient = storageAccount.createCloudBlobClient();
+			container = blobClient.getContainerReference("quickstartcontainer");
+
+			// Create the container if it does not exist with public access.
+			System.out.println("Creating container: " + container.getName());
+			container.createIfNotExists(BlobContainerPublicAccessType.CONTAINER, new BlobRequestOptions(), new OperationContext());
+
+			// Getting a blob reference
+			CloudBlockBlob blob = container.getBlockBlobReference("test1");
+
+			// Creating blob and uploading file to it
+			System.out.println("Uploading the sample file ");
+			blob.uploadFromByteArray(requestBody.getBytes(), 0, requestBody.length());
+		} catch (Exception e) {
+			System.out.println(String.format("Error returned from the service. %s: %s", e.getClass().getCanonicalName(), e.getMessage()));
+		}
+
+		return request.createResponse(200, requestBody);
+	}
+}
